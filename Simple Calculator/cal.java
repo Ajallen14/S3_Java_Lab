29641799993641989fb808b945fcd9874bb26bb3
Allen Jude <ajallen142001@gmail.com>
2024-09-11 21:56:15 +0530

    commit

---
+++ Simple Calculator/cal.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+class Calculator extends JFrame implements ActionListener{
+    private JTextField t1;
+
+    private JButton b1;
+    private JButton b2;
+    private JButton b3;
+    private JButton b4;
+    private JButton b5;
+    private JButton b6;
+    private JButton b7;
+    private JButton b8;
+    private JButton b9;
+    private JButton b10;
+    private JButton b11;
+    private JButton b12;
+    private JButton b13;
+    private JButton b14;
+    private JButton b15;
+    private JButton b16;
+    private JButton b17;
+
+    private Integer res;
+    private  String operation;
+
+    public Calculator(){
+        setLayout(null);
+    }
+}
