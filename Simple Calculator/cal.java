10f0c58a934b534660005663ad0e500031427e11
Allen Jude <ajallen142001@gmail.com>
2024-09-22 13:27:31 +0530

    commit

--- Simple Calculator/cal.java
+++ Simple Calculator/cal.java
@@ -30,9 +30,32 @@ class Calculator extends JFrame implements ActionListener{
         setSize(680, 480);
         t1 = new JTextField();
         t1. setBounds(100, 100, 200, 30);
+
         b1 = new JButton("1");
         b1.setBounds(100, 140, 50, 30);
         b1 = new JButton("2");
         b1.setBounds(150, 140, 50, 30);
+        b3 = new JButton("3");
+        b3.setBounds(200, 140, 50, 30);
+        b4 = new JButton("+");
+
+        b4.setBounds(250, 140, 50, 30);
+        b5 = new JButton("4");
+        b5.setBounds(100, 170, 50, 30);
+        b6 = new JButton("5");
+        b6.setBounds(150, 170, 50, 30);
+        b7 = new JButton("6");
+        b7.setBounds(200, 170, 50, 30);
+        b8 = new JButton("-");
+        b8.setBounds(250, 170, 50, 30);
+
+        b9 = new JButton("7");
+        b9.setBounds(100, 200, 50, 30);
+        b10 = new JButton("8");
+        b10.setBounds(150, 200, 50, 30);
+        b11 = new JButton("9");
+        b11.setBounds(200, 200, 50, 30);
+        b12 = new JButton("x");
+        b12.setBounds(250, 200, 50, 30);
     }
 }
